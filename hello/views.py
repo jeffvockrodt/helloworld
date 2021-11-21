e56213fbcfe0d0a4bf7d97021dd9fca635f8bdc8
Jeff Vockrodt <jeffvockrodt@gmail.com>
2021-11-21 14:45:20 -0500

    mod: hello.views

--- hello/views.py
+++ hello/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return HttpResponse("Hello, Django! Here's a new change I made")
 # Create your views here.
